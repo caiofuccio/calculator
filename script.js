358ea935d9f3f9f4c9a5ab76aa9db09cff76add1
Caio Fuccio <caio.fuccio@gmail.com>
2021-02-07 23:58:20 -0300

    added the delete, appendOperator and calculate functions and finished the calculator functions

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ class Calculator {
     this.previousInputText = previousInputText;
     this.currentInputText = currentInputText;
     this.clear();
+
+    // Flag to determine if current input is a result
+    this.isResult = false;
+  }
+
+  markAsResult() {
+    this.isResult = true;
   }
 
   clear() {
@@ -11,26 +18,74 @@ class Calculator {
     this.currentInput = '';
     this.previousInput = '';
     this.operation = undefined;
+    this.isResult = false;
   }
 
-  delete() {}
+  delete() {
+    // Prevents from deleting digits from results
+    if (this.isResult) return;
 
-  append(number) {
+    this.currentInput = this.currentInput.slice(0, -1);
+  }
+
+  appendNumber(number) {
+    // Prevents user from appending numbers to results
+    if (this.isResult) this.clear();
+    // Prevents user from writing multiple dots
     if (number === '.' && this.currentInput.includes('.')) return;
+    // In case there's no current input, just write the number
     if (this.currentInput === '') {
       this.currentInput = number;
       return;
     }
 
-    this.currentInput = this.currentInput + number;
+    this.currentInput += number;
   }
 
-  getOperation(operation) {}
+  appendOperator(operator) {
+    // In case there's no input to operate, simply return
+    if (this.currentInput === '') return;
+    //In case there's a previous input, calculate the result
+    if (this.previousInput !== '') this.calculate();
 
-  calculate() {}
+    this.operation = operator;
+    this.previousInput = `${this.currentInput} ${this.operation}`;
+    this.currentInput = '';
+  }
+
+  calculate() {
+    let result = 0;
+    const previousOperand = parseFloat(this.previousInput);
+    const currentOperand = parseFloat(this.currentInput);
+
+    // If there's no previous or current operands, just return
+    if (isNaN(previousOperand) || isNaN(currentOperand)) return;
+
+    switch (this.operation) {
+      case '+':
+        result = previousOperand + currentOperand;
+        break;
+      case '-':
+        result = previousOperand - currentOperand;
+        break;
+      case '×':
+        result = previousOperand * currentOperand;
+        break;
+      case '÷':
+        result = previousOperand / currentOperand;
+        break;
+      default:
+        return;
+    }
+
+    this.currentInput = result.toString();
+    this.previousInput = '';
+    this.operation = undefined;
+  }
 
   updateDisplay() {
     this.currentInputText.textContent = this.currentInput;
+    this.previousInputText.textContent = this.previousInput;
   }
 }
 
@@ -49,7 +104,30 @@ const calculator = new Calculator(previousInputText, currentInputText);
 // Event listeners
 numberButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    calculator.append(button.textContent);
+    calculator.appendNumber(button.textContent);
     calculator.updateDisplay();
   });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    calculator.appendOperator(button.textContent);
+    calculator.updateDisplay();
+  });
+});
+
+allClearButton.addEventListener('click', () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+clearButton.addEventListener('click', () => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
+
+equalButton.addEventListener('click', () => {
+  calculator.calculate();
+  calculator.markAsResult();
+  calculator.updateDisplay();
 });
